2618768f66aeb505d61ae49a45be1fef00cab711
Claims <ysfharb@hotmail.com>
2022-10-05 11:03:20 +0300

    Initial commit

---
+++ src/main/java/com/example/springgittest/SpringgittestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgittestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgittestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springgittest/SpringgittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringgittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
